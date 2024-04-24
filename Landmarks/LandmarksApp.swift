0a8ebecdedaf21c6034fe24c915d874192d6ec94
Naim Romero <niromerosand@cps.edu>
2024-04-24 08:32:41 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Romero Sandoval, Naim I on 4/24/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
