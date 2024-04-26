f4f95f9e04f93e8d6e71727a10d3b6476ea0f631
Naim Romero <niromerosand@cps.edu>
2024-04-26 08:21:59 -0500

    ureubfebwf

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -10,10 +10,15 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, worldd!")
+            VStack (alignment: .leading) {
+                Text("Turtle Rock")
+         .font(.title)
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
         }
         .padding()
     }
